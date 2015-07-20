4bf5a0f2ef4d9cbbfa9ce90647dc44eefdd7c5ac
Ashlyn <ashlyna@icracked.com>
2015-07-20 07:42:47 -0700

    heroku new files.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -42,6 +42,6 @@ angular
         controller: 'PrelimbCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '#/'
       });
   });
--- web.js
+++ web.js
@@ -3,5 +3,9 @@ var express = require('express');
 var app = express();
  
 app.use(express.logger('dev'));
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+http.listen(process.env.PORT || 5000, function(){
+});
