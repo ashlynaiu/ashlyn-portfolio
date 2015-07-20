09ab9c8eb32f3a5b4fa543c8b9cb8311ad0e6da3
Ashlyn <ashlyna@icracked.com>
2015-07-20 07:54:21 -0700

    Proc file fix

--- web.js
+++ web.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = express();
  
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + ashlyn-portfolio + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 http.listen(process.env.PORT || 5000, function(){
 });
