3b5e099a75a65bb6a6269693c63f1c62cd07fad8
Ashlyn <ashlyna@icracked.com>
2015-07-20 21:58:16 -0700

    For the love of God and all that is holy, please work.

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var logger = require('express-logger');
 var app = express();
-var port_number = server.listen(process.env.PORT || 5000);
 
-app.use(express.logger('dev'));
+app.use(logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(port_number);
+app.listen(process.env.PORT || 5000);
