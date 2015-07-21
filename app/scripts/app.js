400252b985400ef5ecdb23ebf43590d536555f7a
Ashlyn <ashlyna@icracked.com>
2015-07-21 15:48:12 -0700

    heroku try.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,11 @@ angular
     'ngTouch'
   ])
   .config(function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    $locationProvider.hashPrefix('/');
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
