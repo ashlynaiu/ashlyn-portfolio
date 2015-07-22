54920922ec50783f3b2d79aa42e37619b647f3f4
Ashlyn <ashlyna@icracked.com>
2015-07-22 08:56:45 -0700

    web.js fix

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/*', function(req, res) {
+  res.render('/dist/index.html');
 });
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
