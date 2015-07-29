d19aeea87ad0cd1a56b81f7dda1ee1c517d4c4cf
Ashlyn <ashlyn.aiu@gmail.com>
2015-07-29 16:29:01 -0700

    Adding prerender to node.

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-
+app.use(require('prerender-node').set('prerenderToken', 'VIm8wWFegmX5A1WPs03N'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.get('/*', function(req, res){
