d42bfcf321c7a8b38f7a88cb5a8e92ffb5e762fd
ashlynaiu <ashlyn.aiu@gmail.com>
2015-08-17 09:35:19 -0700

    better form validation

--- app/scripts/controllers/sandbox.js
+++ app/scripts/controllers/sandbox.js
@@ -8,17 +8,6 @@
  * Controller of the ashlynPortfolioApp
  */
 angular.module('ashlynPortfolioApp')
-  .controller('SandboxCtrl', function($scope, $timeout){
-    var statusClasses = ['inactive', 'onFocus', 'onBlur'];
-    var activeColor = 0;
-    $scope.modelStatus = function() {
-        return (statusClasses[activeColor]);
-    };
-    $scope.focusCallback = function() {
-        activeColor = 1;
-    };
-    $scope.blurCallback = function() {
-        activeColor = 2;
-        $timeout(function() {activeColor = 0;}, 2000);
-    };
+  .controller('SandboxCtrl', function($scope){
+    
 });
