e8c5314dd4c0f041f9f3b0c6e16d04c97b6ccb98
ashlynaiu <ashlyn.aiu@gmail.com>
2016-05-12 21:51:59 -0700

    finalizing new homepage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,24 +14,16 @@ angular.module('ashlynPortfolioApp')
         $scope.projects = data;
     });
     function changeBackground() {
-      //change color every frame
-      //initialise colors ( hue, lightness)
-      var hue = 360;
-      var light = 0;
+      var hue = 0;
 
       function changeHue (){
         var col1 = Math.abs((hue % 720) - 360);
         var col2 = Math.abs( ( (hue+90) % 720) - 360);
         hue++ ;
 
-        // //values for light adjustment not used
-        // var light1 = Math.abs( (light % 40) - 20)+60;
-        // var light2 = Math.abs( ( (light+10) % 40) - 20)+60;
-        // light++ ;
-
-        document.getElementById('header-animation').style.background = 'linear-gradient(to right, hsl('+col1 +',70%, 75%) 0%,hsl('+col2 +',90%,75%) 100%)';
+        document.getElementById('header-animation').style.background = 'linear-gradient(to right, hsl('+col1 +',70%, 80%) 0%,hsl('+col2 +',90%, 80%) 100%)';
       }
-      setInterval (changeHue, 64);
+      setInterval (changeHue, 100);
     }
     changeBackground();
 });
