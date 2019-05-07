2f4685db46ac72447570fcedf815509cb4a9ce92
ashlynaiu <ashlyn.aiu@gmail.com>
2019-05-06 20:10:58 -0700

    gradient working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ class App extends Component {
     return (
       <footer>
         <ul>
-          <li><a href="https://github.com/ashlynaiu" target="_blank"><i class="svg-port-social-github"></i></a></li>
-          <li><a href="https://twitter.com/ashlynaiu" target="_blank"><i class="svg-port-social-twitter"></i></a></li>
-          <li><a href="http://singanything.tumblr.com/" target="_blank"><i class="svg-port-social-tumblr"></i></a></li>
-          <li><a href="https://dribbble.com/ashlynaiu" target="_blank"><i class="svg-port-social-dribbble"></i></a></li>
-          <li><a href="https://www.linkedin.com/pub/ashlyn-aiu/42/56b/60a" target="_blank"><i class="svg-port-social-linkedin"></i></a></li>
+          <li><a href="https://github.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i class="svg-port-social-github"></i></a></li>
+          <li><a href="https://twitter.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i class="svg-port-social-twitter"></i></a></li>
+          <li><a href="http://singanything.tumblr.com/" rel="noopener noreferrer" target="_blank"><i class="svg-port-social-tumblr"></i></a></li>
+          <li><a href="https://dribbble.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i class="svg-port-social-dribbble"></i></a></li>
+          <li><a href="https://www.linkedin.com/pub/ashlyn-aiu/42/56b/60a" target="_blank" rel="noopener noreferrer"><i class="svg-port-social-linkedin"></i></a></li>
         </ul>
       </footer>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import data from './models'
-import Project from 'Project'
+//import data from '../models';
+// import Project from './Project'
+// import { isNullOrUndefined } from 'util';
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: ''
+    }
+  }
+  componentWillMount() {
+    console.log(this.state.backgroundColor)
+    let hue = 0;
+      setInterval(() => {
+        var col1 = Math.abs((hue % 720) - 360);
+        var col2 = Math.abs( ( (hue+90) % 720) - 360);
+        hue++;
+        let newColor = 'linear-gradient(to right, hsl('+col1 +',70%, 70%) 0%,hsl('+col2 +',90%, 70%) 100%)';
+        console.log(newColor);
+        this.setState({ backgroundColor: newColor});
+      }, 3000);
+  }
   render() {
-
+    
     return (
-      <div className="mainContent">
+      <div className="mainContent" style={{backgroundImage: this.state.backgroundColor}}>
         <div className="port-view-homepage-header" id="header-animation">
           <h1>Hey, my name is Ashlyn</h1>
-          <div class="port-view-homepage-header-line"></div>
+          <div className="port-view-homepage-header-line"></div>
           <h2>Product Designer in San Francisco</h2>
         </div>
         <section className="port-main-content port-project-layout">
           <div className="port-project-layout-projects">
-          {Object
-            .keys(data)
-            .map(key => <Project
-                key={key}
-                index={key}
-                card={data[key]}/>)
-          }
+          
           </div>
         </section>
       </div>
