b7fa5b1962c6c17a8f8d7db597b8e896758591f6
Ashlyn Aiu <ashlyn.m.watters@gmail.com>
2023-09-28 21:45:54 -0700

    update links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ class App extends Component {
     return (
       <footer>
         <ul>
-          <li><a href="https://twitter.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i className="fab fa-twitter"></i></a></li>
           <li><a href="https://www.linkedin.com/in/ashlyn-watters" rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin"></i></a></li>
           <li><a href="https://github.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i className="fab fa-github"></i></a></li>
         </ul>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,14 @@ class Home extends Component {
     return (
         <div className="homepage">
           <h1>Ashlyn Watters</h1>
-          <h2>Product Designer and UX/UI Engineer</h2>
+          <h2>Product Designer</h2>
           <p className="first">Currently I am a Architect of Product Design for <a href="https://appexchange.salesforce.com/" target="_blank" rel="noopener noreferrer">AppExchange</a>, Salesforce's Enterprise App Marketplace. Before AppExchange I was a Product Design Lead at <a href="https://quip.com/" target="_blank" rel="noopener noreferrer">Quip</a> where I helped the Product team reimagined how collaboration and siloed CRM data might intersect. I was also one of the first designers on <a href="https://www.salesforce.com/products/financial-services-cloud/overview/" target="_blank" rel="noopener noreferrer">Financial Services Cloud</a> where I helped define and design their 5 year roadmap. </p>
 
-          <p>While Product Design is my main focus now, I was previously a UI Engineer at <a href="https://www.zenefits.com/" target="_blank" rel="noopener noreferrer">Zenefits</a> and also got to wear numerous hats at iCracked as a Designer and Engineer. I enjoy being apart of the entire product lifecycle; from inital customer discovery and research to creating React prototypes and high fidelity mocks.</p>
+          <p>While Product Design is my main focus now, I was previously a UI Engineer at <a href="https://www.zenefits.com/" target="_blank" rel="noopener noreferrer">Zenefits</a> and also got to wear numerous hats at iCracked as a Product Designer and Engineer.</p>
 
-          <p>These case studies are just quick glimpses into my favorite problems and solutions I have tackled over the last five years. Please reach out through <a href="https://www.linkedin.com/in/ashlyn-watters" target="_blank" rel="noopener noreferrer">Linkedin</a> to learn more about my process and projects.</p>
+          <p>The work presented on this website is just quick glimpses into projects that have been publically launched.</p>
+          
+          <p><b>To see a more indepth portfolio of more current work, please reach out to me through <a href="https://www.linkedin.com/in/ashlyn-watters" target="_blank" rel="noopener noreferrer">Linkedin</a> to get the password to my <a href="https://www.figma.com/proto/j1mpj6rRfgpHYSn7yJqosp/Portfolio?page-id=0%3A1&type=design&node-id=1-274&viewport=936%2C785%2C0.25&t=yQ6FjJJgxwCQe1EF-1&scaling=scale-down-width&starting-point-node-id=1%3A274&mode=design" target="_blank" rel="noopener noreferrer">extended portfolio</a>.</b></p>
         </div>
     );
   }
