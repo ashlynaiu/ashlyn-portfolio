b99d6a2f583a72ccaf44b959449b52e0dec44a6a
ashlynaiu <ashlyn.m.watters@gmail.com>
2025-05-30 16:18:46 -0700

    add resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import resume from '../images/ashlyn-watters-resume.pdf';
+
 class App extends Component {
   render() {
     return (
       <footer>
-        <ul>
-          <li><a href="https://www.linkedin.com/in/ashlyn-watters" rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin"></i></a></li>
-          <li><a href="https://github.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i className="fab fa-github"></i></a></li>
+        <ul className="footer">
+          <li className="icon"><a href="https://www.linkedin.com/in/ashlyn-watters" rel="noopener noreferrer" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a></li>
+          <li className="icon"><a href="https://github.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i className="fa-brands fa-github-alt"></i></a></li>
+          <li className="resume"><a href={resume} rel="noopener noreferrer" target="_blank"><i className="fa-regular fa-file-pdf"></i><span> R&#233;sum&#233;</span></a></li>
         </ul>
       </footer>
     );
