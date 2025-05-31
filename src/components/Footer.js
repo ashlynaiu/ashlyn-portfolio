59e1fe311302852b11097c8ab6e39d6f734feef2
ashlynaiu <ashlyn.m.watters@gmail.com>
2025-05-30 17:33:41 -0700

    left align icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <footer>
         <ul className="footer">
+          <li className="resume"><a href={resume} rel="noopener noreferrer" target="_blank"><i className="fa-regular fa-file-pdf"></i><span> R&#233;sum&#233;</span></a></li>
           <li className="icon"><a href="https://www.linkedin.com/in/ashlyn-watters" rel="noopener noreferrer" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a></li>
           <li className="icon"><a href="https://github.com/ashlynaiu" rel="noopener noreferrer" target="_blank"><i className="fa-brands fa-github-alt"></i></a></li>
-          <li className="resume"><a href={resume} rel="noopener noreferrer" target="_blank"><i className="fa-regular fa-file-pdf"></i><span> R&#233;sum&#233;</span></a></li>
         </ul>
       </footer>
     );
